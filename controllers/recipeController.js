e8aca296be6be27e5be82d00135dfbf460e1b3df
ayalacohen786 <ac33116878@gmail.com>
2025-02-26 23:01:48 +0200

    1/allRecipe

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -4,16 +4,16 @@ exports.AddRecipe = async(req, res) => {
     const recipe = await Rccipe.create(req.body);
     res.json(recipe)
 }
-// exports.getAllRecipes = async (req, res) => {
-//     try {
+exports.getAllRecipes = async (req, res) => {
+    try {
 
-//       const recipe = await User.find();
-//       res.json(recipe);
-//     } catch (error) {
-//       console.error('Failed to get users:', error);
-//       res.status(500).json({ message: 'Failed to get users' });
-//     }
-//   };
+      const recipe = await User.find();
+      res.json(recipe);
+    } catch (error) {
+      console.error('Failed to get users:', error);
+      res.status(500).json({ message: 'Failed to get users' });
+    }
+  };
 
 
 
